995e67761ef240c3e6f6348278b5ce2ab7f4de67
DanB <danielbarrownz@gmail.com>
2019-09-06 09:05:38 +1200

    applying discount
    apply firast discount band

--- Calculator.js
+++ Calculator.js
@@ -1,32 +1,68 @@
 
-function calculateValue() {
-const itemValue = document.getElementById("itemValue").value;
-const quantity = document.getElementById("quantity").value;
-const taxCode = document.getElementById("taxCode").value; 
-const totalValue = itemValue * quantity;
+function processTransaction() {
+    const itemValue = document.getElementById("itemValue").value;
+    const quantity = document.getElementById("quantity").value;
+    const taxCode = document.getElementById("taxCode").value; 
+    const subTotal = itemValue * quantity;
 
-console.log("itemValue " + itemValue);
-console.log("quantity " + quantity);
-console.log("total " + totalValue);
-const taxRate = chooseTaxRate(taxCode);
-const taxToPay = calculateTax(taxRate);
-calculateGrandTotal(totalValue, taxToPay);
+    console.log("itemValue " + itemValue);
+    console.log("quantity " + quantity);
+    console.log("subtotal " + subTotal);
+
+    const discountRate = chooseDiscountRate(subTotal);
+    console.log("discountRate " + discountRate);
+
+    const discountValue = calculateDiscount(discountRate);
+    console.log("discountValue " + discountValue);
+
+    const taxRate = chooseTaxRate(taxCode);
+    console.log("taxRate " + taxRate);
+
+    const taxToPay = calculateTax(taxRate);
+    console.log("taxToPay " + taxToPay);
+
+    const grandTotal = calculateGrandTotal(discountValue, subTotal, taxToPay);
+    displayResult(grandTotal);
+
 }
+function chooseDiscountRate(subTotal){
+    switch (subTotal){
+        case (subTotal <= 999):
+            return 0;
+        case ((subTotal >= 1000) && (subTotal <= 4999)):
+            return 3;
+        default: 
+            return 0;
+    }
+}
+
+function calculateDiscount(discountRate, subTotal) {
+    return ((subTotal/100)*discountRate);
+}
+
 
 function chooseTaxRate(taxCode) {
-console.log("taxcode " + taxCode);
-switch (taxCode) {
-    case "UT":
-        return 6.85;
-    default: 
-        return 0;
- }
+    console.log("taxcode " + taxCode);
+    switch (taxCode) {
+        case "UT":
+            return 6.85;
+        default: 
+            return 0;
+    }
 }
+
+
 
 function calculateTax(taxRate) {
 
 }
 
-function calculateGrandTotal(totalValue, taxToPay, taxCode) {
-alert("Transaction total: " + totalValue + ", Tax Code: " + taxCode); // i don't understand why taxCode only works without declaring it as a 'var' or other type.
+function calculateGrandTotal(subTotal, discountValue) {
+return subTotal - discountValue;
+}
+
+function displayResult(grandTotal){
+    alert("Transaction total: " + grandTotal); // i don't understand why taxCode only works without declaring it as a 'var' or other type.
+
+    
 }
