0f47a861d8c44cc5d518310f0225b5cd37aa18b8
DanB <danielbarrownz@gmail.com>
2019-09-05 14:06:49 +1200

    changes with lance

--- Calculator.js
+++ Calculator.js
@@ -1,29 +1,32 @@
-var totalValue;
-var taxToPay;
 
 function calculateValue() {
-let itemValue = document.getElementById("itemValue").value;
-let quantity = document.getElementById("quantity").value;
-totalValue = itemValue * quantity;
-
+const itemValue = document.getElementById("itemValue").value;
+const quantity = document.getElementById("quantity").value;
+const taxCode = document.getElementById("taxCode").value; 
+const totalValue = itemValue * quantity;
 
 console.log("itemValue " + itemValue);
 console.log("quantity " + quantity);
 console.log("total " + totalValue);
-
-calculateTax();
-calculateGrandTotal();
+const taxRate = chooseTaxRate(taxCode);
+const taxToPay = calculateTax(taxRate);
+calculateGrandTotal(totalValue, taxToPay);
 }
 
-function calculateTax() {
-taxCode = document.getElementById("taxCode").value; 
+function chooseTaxRate(taxCode) {
 console.log("taxcode " + taxCode);
+switch (taxCode) {
+    case "UT":
+        return 6.85;
+    default: 
+        return 0;
+ }
+}
 
+function calculateTax(taxRate) {
 
 }
 
-function calculateGrandTotal() {
-
-confirm("Transaction total: " + totalValue + ", Tax Code: " + taxCode); // i don't understand why taxCode only works without declaring it as a 'var' or other type.
-
+function calculateGrandTotal(totalValue, taxToPay, taxCode) {
+alert("Transaction total: " + totalValue + ", Tax Code: " + taxCode); // i don't understand why taxCode only works without declaring it as a 'var' or other type.
 }
